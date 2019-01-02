a9150e482eea99630999bcfeaf541ddade355f82
Alex <alexfosterjr@gmail.com>
2019-01-02 18:19:26 -0500

    final letter guess game

---
+++ assets/javascript/game.js
@@ -0,0 +1,55 @@
+var letters = ["A", "B", "C", "D", "E", "F", "G", 
+"H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", 
+"R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+var mysteryLetter = letters[Math.floor(Math.random() * letters.length)];
+   document.onkeypress = function(event){
+    var  userGuess = event.key
+
+   
+    var guesses = [];
+//      // setting guesses left
+   var guessesLeft = 10;
+   var wins =0;
+   var losses=0;
+   var winner = document.getElementById("wins");
+   var loser = document.getElementById("losses")
+   var guessedLetters = document.getElementById("guessedLetters");
+   var numGuessLeft = document.getElementById("numGuessLeft");
+
+
+  
+   var mysteryLetter = letters[Math.floor(Math.random() * letters.length)];
+   document.onkeypress = function(event){
+    var  userGuess = event.key
+ console.log(mysteryLetter);
+      if(userGuess === mysteryLetter){
+          wins = wins + 1;
+          winner.textContent = "Wins: " + wins;
+          guesses = [];
+          guessesLeft = 10;
+          numGuessLeft.textContent = "Guesses Left: " + guessesLeft;
+          mysteryLetter = letters[Math.floor(Math.random() * letters.length)];
+
+      }
+      else{
+          guessesLeft = guessesLeft - 1;
+          numGuessLeft.textContent = "Guesses Left: " + guessesLeft;
+          guesses.push(userGuess);
+          guessedLetters.textContent = "Previous Guesses: " + guesses;
+      }
+      
+      if (guessesLeft == 0) {
+        mysteryLetter = letters[Math.floor(Math.random() * letters.length)];
+        losses = losses + 1;
+        loser.textContent = "Losses: " + losses;
+        guesses = []
+        guessesLeft = 10;
+        
+        
+      }
+
+    }
+}
+
+
+// });
